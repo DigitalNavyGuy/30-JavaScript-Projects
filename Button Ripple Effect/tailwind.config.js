012df0356a2b47240e155a33bcbeb8598196e4dd
John Fuller <fuller.john84@gmail.com>
2025-02-26 17:13:08 -0500

    First commit.

---
+++ Button Ripple Effect/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "custom-light": "0 4px 8px rgba(0, 0, 0, 0.5)", // Custom shadow example
+      },
+    },
+  },
+  plugins: [],
+};
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 30-JavaScript-Projects
+
+# This is a collection of small JavaScript projects to learn, practice and for later use.
